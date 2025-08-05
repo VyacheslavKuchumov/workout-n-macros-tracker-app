d66bba19fe28959e46a701b806c5181773a9b662
VyacheslavKuchumov <87779022+VyacheslavKuchumov@users.noreply.github.com>
2025-08-05 16:02:12 +0500

    wip(accounts): Create view and serializers for me/

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+class UserDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
